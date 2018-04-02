eea9f1147f387d84bd5ea2469b1b44526cb41911
Peter Szalai <peter.szalai@prezi.com>
2018-04-02 16:13:43 +0200

    interfaces add 7.1 7.2 solutions

--- ch7/bytecounter/main.go
+++ ch7/bytecounter/main.go
@@ -7,7 +7,10 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 )
 
 //!+bytecounter
@@ -17,6 +20,36 @@ type ByteCounter int
 func (c *ByteCounter) Write(p []byte) (int, error) {
 	*c += ByteCounter(len(p)) // convert int to ByteCounter
 	return len(p), nil
+}
+
+// exercise 7.1
+type WordCounter int
+
+func (c *WordCounter) Write(p []byte) (int, error) {
+	sc := bufio.NewScanner(bytes.NewReader(p))
+	sc.Split(bufio.ScanWords)
+	for sc.Scan() {
+		*c++
+	}
+	return len(p), nil
+}
+
+// ecercise 7.2
+type countingWriter struct {
+	w io.Writer
+	c *int64
+}
+
+func (cw *countingWriter) Write(p []byte) (int, error) {
+	n, err := cw.w.Write(p)
+	*(cw.c) += int64(n)
+	return n, err
+}
+
+func CountingWriter(w io.Writer) (io.Writer, *int64) {
+	cw := &countingWriter{w: w, c: new(int64)}
+	*(cw.c) = int64(0)
+	return cw, cw.c
 }
 
 //!-bytecounter
@@ -32,4 +65,14 @@ func main() {
 	fmt.Fprintf(&c, "hello, %s", name)
 	fmt.Println(c) // "12", = len("hello, Dolly")
 	//!-main
+
+	// exercise 7.1
+	var wc WordCounter
+	cw, n := CountingWriter(&wc)
+	_, err := fmt.Fprint(cw, "1 2 3 44444\n55\n66")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("numberOfWords: %d\n", wc)
+	fmt.Printf("cw.c: %d\n", *n)
 }
